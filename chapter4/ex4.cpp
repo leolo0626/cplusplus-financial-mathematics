7fa9beb4dbcc85f0dfd7a1c63470cee12e938a1d
Leo Lo <leolo0626@gmail.com>
2022-09-11 23:09:10 +0800

    chapter4 ex

---
+++ chapter4/ex4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std; 
+
+
+double integralWithRectangleRule(double (*func)(double), double start, double end, double n);
+
+double functionToIntegrate(double x);
+
+int main() {
+    cout << "Integral with rectangle rule sin(x) " << integralWithRectangleRule(&functionToIntegrate, 0, 2, 100000000) << "\n";
+    return 0; 
+}
+
+double functionToIntegrate(double x) {
+    // cout << "Process " << x << "\n";
+    return x; 
+}
+
+double integralWithRectangleRule(double (*func)(double), double start, double end, double n) {
+    double h = (end - start) / n ;
+    double sumValue = 0;
+    
+    for (int i = 0; i < n; i++) {
+        double params = start + i * h + 0.5 * h;
+        sumValue += func(params);
+    }
+    return sumValue/n;
+}
